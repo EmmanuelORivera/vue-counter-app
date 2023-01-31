be2fe85a742a1aeb03ae47676a59b9fba38023e6
Emmanuel Ochoa <eorivera95@gmail.com>
2023-01-30 22:20:18 -0600

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div>
+      <h4>The current count is ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+}
+div {
+  text-align: center;
+}
+</style>
